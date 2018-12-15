7b02b4893d621162cbf4816731d87db7a4809eee
Ilya <ilya.mayorov.2003@list.ru>
2018-12-15 13:42:00 +0300

    Start

---
+++ Coder.py
@@ -0,0 +1,277 @@
+import json
+
+
+def Text_up(text):
+    return list(map(lambda x: x.upper(), text.split()))
+
+
+def Crypt_Caesar(text, key=1):
+    text = Text_up(text)
+
+    text_true = []
+    for word in text:
+        final = ''
+        for symbol in word:
+            final += chr((ord(symbol) + key - 13) % 26 + ord('A'))
+        text_true.append(final)
+
+    if len(text_true) != 0:
+        return ' '.join(text_true)
+    else:
+        return 'Введите данные корректно'
+
+
+def Decrypt_Caesar(text, key=1):
+    text = Text_up(text)
+
+    text_true = []
+    for word in text:
+        final = ''
+        for symbol in word:
+            final += chr((ord(symbol) - key - 13) % 26 + ord('A'))
+        text_true.append(final)
+    if len(text_true) != 0:
+        return ' '.join(text_true)
+    else:
+        return 'Введите данные корректно'
+
+
+def Decrypt_Vegenere(text, key='A'):
+    text_full = Text_up(text)
+    text_true = []
+    for word in text_full:
+        key *= len(word) // len(key) + 1
+        d = ""
+        key = key.upper()
+        for i, j in enumerate(word):
+            gg = (ord(j) + ord(key[i]))
+            d += chr(gg % 26 + 65)
+        text_true.append(d)
+
+    if len(text_true) != 0:
+        return ' '.join(text_true)
+    else:
+        return 'Введите данные корректно'
+
+
+def Crypt_Vigenere(text, key='A'):
+    text_full = Text_up(text)
+    text_true = []
+    for word in text_full:
+        key *= len(word) // len(key) + 1
+        d = ""
+        key = key.upper()
+        for i, j in enumerate(word):
+            gg = (ord(j) - ord(key[i]))
+            d += chr(gg % 26 + 65)
+        text_true.append(d)
+
+    if len(text_true) != 0:
+        return ' '.join(text_true)
+    else:
+        return 'Введите данные корректно'
+
+
+def regulation(text, y=65):
+    text_true = ''
+    for n, i in enumerate(text):
+        text_true += i
+        if (n + 1) % y == 0:
+            text_true += '\n'
+    if len(text_true) != 0:
+        return text_true
+    else:
+        return 'Введите данные корректно'
+
+
+def Crypt_Morze(text):
+    text = text.upper()
+    with open('import_info.json', 'r', encoding='utf-8') as f_obj:
+        CODE = json.load(f_obj)['Morze']
+
+    return ' '.join(CODE.get(i) for i in text)
+
+
+def Decrypt_Morze(text):
+    with open('import_info.json', 'r', encoding='utf-8') as f_obj:
+        CODE = json.load(f_obj)['Morze']
+    CODE_REVERSED = {value: key for key, value in CODE.items()}
+
+    return ''.join(CODE_REVERSED.get(i) for i in text.split())
+
+
+def Crypt_replace(text):
+    text = Text_up(text)
+    text_true = []
+    with open('import_info.json', 'r', encoding='utf-8') as f_obj:
+        keys = json.load(f_obj)['Replaces_keys']
+
+    for word in text:
+        crypt = ""
+        for i in word:
+            if i in keys:
+                crypt += keys[i]
+        text_true.append(crypt)
+
+    if len(text_true) != 0:
+        return ' '.join(text_true).replace(' ', '~')
+    else:
+        return 'Введите корректно'
+
+
+def Decrypt_replace(text):
+    text = text.split()
+    text_true = []
+    with open('import_info.json', 'r', encoding='utf-8') as f_obj:
+        keys_0 = json.load(f_obj)['Replaces_keys']
+    keys = {value: key for key, value in keys_0.items()}
+    for word in text:
+        decrypt = ""
+        for i in word:
+            if i in keys:
+                decrypt += keys[i]
+        text_true.append(decrypt)
+
+    if len(text_true) != 0:
+        return ''.join(text_true).replace('~', ' ')
+    else:
+        return 'Введите корректно'
+
+
+def Crypt_omofon(text):
+    from random import randint
+    text = Text_up(text)
+    print(text)
+    crypt_full = []
+    with open('import_info.json', 'r', encoding='utf-8') as f_obj:
+        keys = json.load(f_obj)['Omofon_keys']
+
+    for word in text:
+        crypt = ""
+        for i in word:
+            if i in keys:
+                lenght = len(keys[i])
+                crypt += keys[i][randint(0, lenght - 1)]
+        crypt_full.append(crypt)
+    if len(crypt_full) != 0:
+        return " ".join(crypt_full)
+    else:
+        return 'Введите корректно'
+
+
+def Decrypt_omofon(text):
+    text = text.split()
+    decrypt_full = []
+    with open('import_info.json', 'r', encoding='utf-8') as f_obj:
+        keys = json.load(f_obj)['Omofon_keys']
+    for word in text:
+        decrypt = ""
+        for i in word:
+            for j in keys:
+                if i in keys[j]:
+                    decrypt += j
+        decrypt_full.append(decrypt)
+    if len(decrypt_full) != 0:
+        return " ".join(decrypt_full)
+    else:
+        return 'Введите корректно'
+
+
+def Crypt_picture(file, password):
+    import pyAesCrypt
+    from os import remove
+    bufferSize = 64 * 1024
+    pyAesCrypt.encryptFile(str(file), str(file) + ".crp", password, bufferSize)
+    remove(file)
+
+
+def Decrypt_picture(file, password):
+    import pyAesCrypt
+    from os import remove
+    from os.path import splitext
+    bufferSize = 64 * 1024
+    pyAesCrypt.decryptFile(str(file), str(splitext(file)[0]), password, bufferSize)
+    remove(file)
+
+
+def Decrypt_Gronsfeld(text, key):
+    text_true = []
+    text = Text_up(text)
+    for message in text:
+        key_1 = key
+        key_1 *= len(message) // len(key) + 1
+        final = ''
+        for index, symbol in enumerate(message):
+            temp = ord(symbol) - int(key_1[index]) - 13
+            final += chr(temp % 26 + ord('A'))
+        text_true.append(final)
+    if len(text_true) != 0:
+        return ' '.join(text_true)
+    else:
+        return 'Введите данные корректно'
+
+
+def Crypt_Gronsfeld(text, key):
+    text_true = []
+    text = Text_up(text)
+    for message in text:
+        key_1 = key
+        key_1 *= len(message) // len(key) + 1
+        final = ''
+        for index, symbol in enumerate(message):
+            temp = ord(symbol) + int(key_1[index]) - 13
+            final += chr(temp % 26 + ord('A'))
+        text_true.append(final)
+
+    if len(text_true) != 0:
+        return ' '.join(text_true)
+    else:
+        return 'Введите данные корректно'
+
+
+def Crypt_pseudosim(text):
+    with open('import_info.json', 'r', encoding='utf-8') as f_obj:
+        keys = json.load(f_obj)['Pseudosim_keys']
+    text_true = []
+    text = Text_up(text)
+    for message in text:
+        final = ''
+        for symbol in message:
+            if symbol in keys:
+                final += keys[symbol]
+        text_true.append(final)
+
+    if len(text_true) != 0:
+        return ''.join(text_true)
+    else:
+        return 'Введите данные коректно'
+
+
+def Decrypt_pseudosim(text):
+    def regular(text):
+        from re import findall
+        template = r"\w{3}"
+        return findall(template, text)
+
+    with open('import_info.json', 'r', encoding='utf-8') as f_obj:
+        keys = json.load(f_obj)['Pseudosim_keys']
+    final = ''
+    for threeSymbols in regular(text):
+        for key in keys:
+            if threeSymbols == keys[key]:
+                final += key
+
+    if len(final) != 0:
+        return final
+    else:
+        return 'Введите данные коректно'
+
+
+def Derypt_Binary_code(bits, encoding='utf-8', errors='surrogatepass'):
+    n = int(bits, 2)
+    return n.to_bytes((n.bit_length() + 7) // 8, 'big').decode(encoding, errors) or '\0'
+
+
+def Crypt_Binary_code(text, encoding='utf-8', errors='surrogatepass'):
+    bits = bin(int.from_bytes(text.encode(encoding, errors), 'big'))[2:]
+    return bits.zfill(8 * ((len(bits) + 7) // 8))
---
+++ Main_file.py
@@ -0,0 +1,256 @@
+import sys, json, pyperclip
+from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton, QInputDialog
+from PyQt5 import QtWidgets, QtCore, uic
+from PyQt5.QtGui import QIcon
+import urllib.request
+from Coder import *
+
+
+class Window1(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('coder.ui', self)
+        self.setWindowIcon(QIcon('RM_icon.jpg'))
+        self.encode = 'Не выбрано'
+        self.info_text = ''  # информация по выбранному шифрованию
+        self.final_text = 'None'  # скопированный текст
+        with open('import_info.json', 'r', encoding='utf-8') as f_obj:
+            self.key_dict = json.load(f_obj)['info']  # считываем инфу по шифровкам и ключи
+        self.pushButton.clicked.connect(self.Dialog_run)
+        self.pushButton_2.clicked.connect(self.crypt_run)
+        self.pushButton_3.clicked.connect(self.decrypt_run)
+        self.pushButton_4.clicked.connect(self.copy_crypt)
+
+    def Dialog_run(self):
+        i, okBtnPressed = QInputDialog.getItem(
+            self,
+            "Выбор кодировки",
+            "Выберите кодировку?",
+            ("Шифр Цезаря", "Шифр Виженера", "Азбука Морзе", "Шифр замены", "Омофонический шифр",
+             "Шифрование Гронсвельда", "Псевдосимвольное шифрование", "Бинарный код"),
+            1,
+            False
+        )
+        if okBtnPressed:
+            self.encode = i
+            self.label_2.setText(i)  # вставляем название кодировке
+            if self.encode == "Не выбрано":
+                self.label_8.setText("Кодировка не выбрана")  # вставляем инфу о шифре
+            elif self.encode == 'Шифр Цезаря':
+                self.info_text = regulation(self.key_dict['Шифр Цезаря'], 26)
+                self.label_8.setText(self.info_text)
+            elif self.encode == 'Шифр Виженера':
+                self.info_text = regulation(self.key_dict['Шифр Виженера'], 26)
+                self.label_8.setText(regulation(self.info_text))
+            elif self.encode == 'Азбука Морзе':
+                self.info_text = regulation(self.key_dict['Азбука Морзе'], 26)
+                self.label_8.setText(self.info_text)
+            elif self.encode == 'Шифр замены':
+                self.info_text = regulation(self.key_dict['Шифр замены'], 26)
+                self.label_8.setText(self.info_text)
+            elif self.encode == "Омофонический шифр":
+                self.info_text = regulation(self.key_dict["Омофонический шифр"], 26)
+                self.label_8.setText(self.info_text)
+            elif self.encode == "Бинарный код":
+                self.info_text = regulation(self.key_dict["Бинарный код"], 26)
+                self.label_8.setText(self.info_text)
+            elif self.encode == "Шифрование Гронсвельда":
+                self.info_text = regulation(self.key_dict["Шифрование Гронсвельда"], 26)
+                self.label_8.setText(self.info_text)
+            elif self.encode == "Псевдосимвольное шифрование":
+                self.info_text = regulation(self.key_dict["Псевдосимвольное шифрование"], 26)
+                self.label_8.setText(self.info_text)
+
+    def crypt_run(self):
+        try:
+            if self.encode == "Не выбрано":
+                pass
+            elif self.encode == "Шифр Цезаря":
+                self.final_text = Crypt_Caesar(self.textEdit.toPlainText(), int(self.lineEdit.text()))
+                self.label_6.setText(regulation(self.final_text))
+            elif self.encode == "Шифр Виженера":
+                self.final_text = Crypt_Vigenere(self.textEdit.toPlainText(), self.lineEdit.text())
+                self.label_6.setText(regulation(self.final_text))
+            elif self.encode == "Азбука Морзе":
+                self.final_text = Crypt_Morze(self.textEdit.toPlainText())
+                self.label_6.setText(regulation(self.final_text))
+            elif self.encode == "Шифр замены":
+                self.final_text = Crypt_replace(self.textEdit.toPlainText())
+                self.label_6.setText(regulation(self.final_text))
+            elif self.encode == "Омофонический шифр":
+                self.final_text = Crypt_omofon(self.textEdit.toPlainText())
+                self.label_6.setText(regulation(self.final_text))
+            elif self.encode == "Шифрование Гронсвельда":
+                self.final_text = Crypt_Gronsfeld(self.textEdit.toPlainText(), self.lineEdit.text())
+                self.label_6.setText(regulation(self.final_text))
+            elif self.encode == "Псевдосимвольное шифрование":
+                self.final_text = Crypt_pseudosim(self.textEdit.toPlainText())
+                self.label_6.setText(regulation(self.final_text))
+            elif self.encode == "Бинарный код":
+                self.final_text = Crypt_Binary_code(self.textEdit.toPlainText())
+                self.label_6.setText(regulation(self.final_text))
+        except Exception:
+            self.label_6.setText('Что-то не так... Проверьте правильность введённых данных')
+
+    def decrypt_run(self):
+        try:
+            if self.encode == "Не выбрано":
+                pass
+            elif self.encode == "Шифр Цезаря":
+                self.final_text = Decrypt_Caesar(self.textEdit.toPlainText(), int(self.lineEdit.text()))
+                self.label_6.setText(regulation(self.final_text))
+            elif self.encode == "Шифр Виженера":
+                self.final_text = Decrypt_Vegenere(self.textEdit.toPlainText(), self.lineEdit.text())
+                self.label_6.setText(regulation(self.final_text))
+            elif self.encode == "Азбука Морзе":
+                self.final_text = Decrypt_Morze(self.textEdit.toPlainText())
+                self.label_6.setText(regulation(self.final_text))
+            elif self.encode == "Шифр замены":
+                self.final_text = Decrypt_replace(self.textEdit.toPlainText())
+                self.label_6.setText(regulation(self.final_text))
+            elif self.encode == "Омофонический шифр":
+                self.final_text = Decrypt_omofon(self.textEdit.toPlainText())
+                self.label_6.setText(regulation(self.final_text))
+            elif self.encode == "Шифрование Гронсвельда":
+                self.final_text = Decrypt_Gronsfeld(self.textEdit.toPlainText(), self.lineEdit.text())
+                self.label_6.setText(regulation(self.final_text))
+            elif self.encode == "Псевдосимвольное шифрование":
+                self.final_text = Decrypt_pseudosim(self.textEdit.toPlainText())
+                self.label_6.setText(regulation(self.final_text))
+            elif self.encode == "Бинарный код":
+                self.final_text = Derypt_Binary_code((self.textEdit.toPlainText()))
+                self.label_6.setText(regulation(self.final_text))
+        except Exception:
+            self.label_6.setText('Что-то не так... Проверьте правильность введённых данных')
+
+    def copy_crypt(self):
+        pyperclip.copy(self.final_text)  # копирует в буфер обмена
+
+
+class Window2(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('ip.ui', self)
+        self.setWindowIcon(QIcon('RM_icon.jpg'))
+        self.pushButton.clicked.connect(self.run)
+
+    def run(self):
+
+        self.label_2.setText('')
+        getIP = self.lineEdit.text()
+
+        try:
+            url = "https://ipinfo.io/" + getIP + "/json"  # создаём ссылочку
+            getInfo = urllib.request.urlopen(url)  # забираем json
+            infoList = json.load(getInfo)  # читаем его
+            self.label_2.setText(
+                "IP: {}\nCity: {}\nRegion: {}\nCountry: {}\nHostname: {}".format(infoList["ip"], infoList["city"],
+                                                                                 infoList["region"],
+                                                                                 infoList["country"], infoList["loc"]))
+        except:
+            self.label_2.setText("Что-то пошло не так :( Проверьте введённые данные")
+
+
+class Window3(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('number.ui', self)
+        self.setWindowIcon(QIcon('RM_icon.jpg'))
+        self.pushButton.clicked.connect(self.run)
+
+    def run(self):
+
+        self.label_2.setText('')
+        phone = self.lineEdit.text()
+        try:
+            getInfo = "https://htmlweb.ru/geo/api.php?json&telcod=" + phone  # создаём ссылочку
+            infoPhone = urllib.request.urlopen(getInfo)  # забираем json
+            infoPhone = json.load(infoPhone)  # читаем его
+            self.label_2.setText("Number: +{}\nCountry: {}\nOkrug: {}\nOperator: {}\nWorld: {}".format(phone, infoPhone[
+                "country"]["name"], infoPhone["region"]["name"], infoPhone["region"]["okrug"], infoPhone["0"]["oper"],
+                                                                                                       infoPhone[
+                                                                                                           "country"][
+                                                                                                           "location"]))
+        except Exception:
+            self.label_2.setText("Что-то пошло не так :( Проверьте введённые данные")
+
+
+class Window4(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('file.ui', self)
+        self.setWindowIcon(QIcon('RM_icon.jpg'))
+        self.pushButton.clicked.connect(self.crypt_run)
+        self.pushButton_2.clicked.connect(self.decrypt_run)
+        with open('import_info.json', 'r', encoding='utf-8') as f_obj:
+            self.label_2.setText(json.load(f_obj)['info']["Шифровка файла"])
+
+    def crypt_run(self):
+        name = self.lineEdit.text()
+        i, okBtnPressed = QInputDialog.getText(
+            self, "Ввод пароля", "Введите пароль"
+        )
+        if okBtnPressed:
+            try:
+                Crypt_picture(name, i)  # шифруем файл
+            except Exception:
+                self.label_3.setText('Ошибка\nПроверьте введённые данные')
+        else:
+            self.label_3.setText('Вы не ввели пароль')
+
+    def decrypt_run(self):
+        name = self.lineEdit.text()
+        i, okBtnPressed = QInputDialog.getText(
+            self, "Ввод пароля", "Введите пароль"
+        )
+        if okBtnPressed:
+            try:
+                Decrypt_picture(name, i)  # дешифруем файл
+            except Exception:
+                self.label_3.setText('Ошибка\nПроверьте введённые данные')
+        else:
+            self.label_3.setText('Вы не ввели пароль')
+
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        self.setGeometry(600, 300, 400, 350)
+        self.setWindowTitle('CIN')
+        self.setWindowIcon(QIcon('RM_icon.jpg'))
+        self.pushButton = QPushButton('Шифровать сообщение', self)
+        self.pushButton.setGeometry(QtCore.QRect(40, 80, 145, 81))
+        self.pushButton_2 = QtWidgets.QPushButton('Пробить по ip', self)
+        self.pushButton_2.setGeometry(QtCore.QRect(40, 200, 145, 81))
+        self.pushButton_3 = QtWidgets.QPushButton("Пробить по номеру", self)
+        self.pushButton_3.setGeometry(QtCore.QRect(220, 200, 145, 81))
+        self.pushButton_4 = QPushButton('Зашифровать файл', self)
+        self.pushButton_4.setGeometry(QtCore.QRect(220, 80, 145, 81))
+        self.pushButton.clicked.connect(self.show_window_1)
+        self.pushButton_2.clicked.connect(self.show_window_2)
+        self.pushButton_3.clicked.connect(self.show_window_3)
+        self.pushButton_4.clicked.connect(self.show_window_4)
+
+    def show_window_1(self):
+        self.w1 = Window1()
+        self.w1.show()
+
+    def show_window_2(self):
+        self.w2 = Window2()
+        self.w2.show()
+
+    def show_window_3(self):
+        self.w3 = Window3()
+        self.w3.show()
+
+    def show_window_4(self):
+        self.w1 = Window4()
+        self.w1.show()
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    w = MainWindow()
+    w.show()
+    sys.exit(app.exec_())
